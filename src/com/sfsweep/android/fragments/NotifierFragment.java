77ac94bf9bd37a8b4269241d981e5b0d209dd358
Adam Brown <arbrow@gmail.com>
2014-04-28 18:39:07 -0700

    Issue #5: Refactoring notification as a two-part spinner, allowing user to specify preferred interval (viz., minute, hour, day) on a spinner-by-spinner basis. \nIssue #8: System alarm temporarily inoperative due to notification refactoring

--- src/com/sfsweep/android/fragments/NotifierFragment.java
+++ src/com/sfsweep/android/fragments/NotifierFragment.java
@@ -1,5 +1,7 @@
 package com.sfsweep.android.fragments;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 
 import android.app.Activity;
@@ -19,19 +21,34 @@ import android.widget.Spinner;
 
 import com.sfsweep.android.R;
 import com.sfsweep.android.activities.MapActivity;
-import com.sfsweep.android.helpers.SpinnerSelectionConverter;
+import com.sfsweep.android.adapters.NotifierIntervalAdapter;
 import com.sfsweep.android.helpers.StreetSweeperDataConverter;
 
-public abstract class NotifierFragment extends Fragment {
+public class NotifierFragment extends Fragment {
 
 	private static final String SPINNER_PREFERENCES = "spinner_preferences"; 
+    private static final String SELECTED_INTERVAL   = "spinner_interval_selection";
+    private static final String SELECTED_MINUTES    = "spinner_minutes_selection";
+    private static final String SELECTED_HOURS      = "spinner_hours_selection";
+    private static final String SELECTED_DAYS       = "spinner_days_selection";
+    
+    private static int sSystemCallsToOnItemSelected = 0; 	// This (ungainly) flag counteracts Android's firing onItemSelected() upon Spinner instantiation, rather than waiting for user interaction (see http://stackoverflow.com/questions/2562248/android-how-to-keep-onitemselected-from-firing-off-on-a-newly-instantiated-spin)
 	
-	private Spinner mSpnNotifier; 
-	private ArrayAdapter<CharSequence> mAdapter;
+	private Spinner mSpnInterval, 
+	                mSpnNumber;
+	private ArrayAdapter<CharSequence> mNotifierIntervalAdapter, 
+	                                   mNotifierMinutesAdapter,
+	                                   mNotifierHoursAdapter,
+	                                   mNotifierDaysAdapter; 
 	private String mSpinnerSelectionKey; 
-	private int mSpinnerValue;
+	private int    mSelectedInterval, 
+	               mSelectedMinutes,
+	               mSelectedHours,
+	               mSelectedDays; 
+	private SharedPreferences mPrefs; 
 	private OnScheduleAlarmCallbacks mListener; 
 	
+
 	public interface OnScheduleAlarmCallbacks {
 		public String onSweepTimeRange(); 
 		public String onDaysToNextSweep();
@@ -60,46 +77,121 @@ public abstract class NotifierFragment extends Fragment {
 	}
 	
 	private void setupWidgets(View v) {
-		mSpnNotifier = (Spinner) v.findViewById(R.id.spnNotifier); 
-		int spinnerValues = getSpinnerValues(); 
-		mAdapter = ArrayAdapter.createFromResource(getActivity(), spinnerValues, 
-				R.layout.spinner_item_custom);
-		mAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item_custom); 
-		mSpnNotifier.setAdapter(mAdapter); 
+		// Set up interval spinner 
+		mSpnInterval = (Spinner) v.findViewById(R.id.spnInterval); 
+			// Implement using ArrayList to enable mutability (e.g., for purposes of changing 
+			// interval from singular to plural)
+		CharSequence[] intervals = {"minutes", "hours", "days"};
+		ArrayList<CharSequence> list = new ArrayList<CharSequence>(); 
+		list.addAll(Arrays.asList(intervals)); 
+		mNotifierIntervalAdapter = new NotifierIntervalAdapter(getActivity(), list, this); 
+		mNotifierIntervalAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item_custom);
+		mSpnInterval.setAdapter(mNotifierIntervalAdapter); 
 		
-		// Restore saved spinner values
-		SharedPreferences prefs = getActivity().getSharedPreferences(SPINNER_PREFERENCES, 0); 
-		mSpinnerSelectionKey = getSpinnerSelectionKey();
-		int position = prefs.getInt(mSpinnerSelectionKey, 0); 
-		mSpnNotifier.setSelection(position); 
+			// Restore pre-selected interval, if any
+		mPrefs = getActivity().getSharedPreferences(SPINNER_PREFERENCES, 0); 
+		int intervalPosition = mPrefs.getInt(SELECTED_INTERVAL, 0);
+		mSpnInterval.setSelection(intervalPosition); 
+		
+		// Set up number spinner
+		mSpnNumber = (Spinner) v.findViewById(R.id.spnNumber); 
+		updateNumberSpinner(intervalPosition, mPrefs);
+	}
+	
+	private void updateNumberSpinner(int intervalPosition, SharedPreferences prefs) {
+		// Retrieve pre-selected interval and create appropriate number adapter
+		ArrayAdapter<CharSequence> adapter; 
+		switch (intervalPosition) {
+		case 0: 
+			if (mNotifierMinutesAdapter == null) {
+				mNotifierMinutesAdapter = ArrayAdapter.createFromResource(getActivity(), 
+					R.array.spn_options_minutes, R.layout.spinner_item_custom); 
+				mNotifierMinutesAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item_custom);
+			}
+			adapter = mNotifierMinutesAdapter; 
+			break;
+		case 1: 
+			if (mNotifierHoursAdapter == null) {
+				mNotifierHoursAdapter = ArrayAdapter.createFromResource(getActivity(), 
+					R.array.spn_options_hours, R.layout.spinner_item_custom);
+				mNotifierHoursAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item_custom);
+			}
+			adapter = mNotifierHoursAdapter;
+			break;
+		default: 
+			if (mNotifierDaysAdapter == null) {
+				mNotifierDaysAdapter = ArrayAdapter.createFromResource(getActivity(), 
+					R.array.spn_options_days, R.layout.spinner_item_custom); 
+				mNotifierDaysAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item_custom); 
+			}
+			adapter = mNotifierDaysAdapter; 
+		}
+		mSpnNumber.setAdapter(adapter); 
+		
+			// Restore pre-selected number, if any
+		mSelectedMinutes = prefs.getInt(SELECTED_MINUTES, 0);
+		mSelectedHours   = prefs.getInt(SELECTED_HOURS, 0);
+		mSelectedDays    = prefs.getInt(SELECTED_DAYS, 0); 
+		int numberPosition = 0;
+		switch (intervalPosition) {
+		case 0:
+			numberPosition = mSelectedMinutes;
+			break;
+		case 1: 
+			numberPosition = mSelectedHours;
+			break;
+		default:
+			numberPosition = mSelectedDays; 
+		}
+		mSpnNumber.setSelection(numberPosition); 
+		adapter.notifyDataSetChanged(); 
 	}
 	
 	private void setupListeners() {
-		mSpnNotifier.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			@Override 
+		mSpnInterval.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-				mSpinnerValue = getSelectedSpinnerItem(position); 
+				if (position != mSelectedInterval) {
+					// Save currently selected interval
+					mSelectedInterval = position;
+					mPrefs.edit().putInt(SELECTED_INTERVAL, mSelectedInterval).commit(); 
+					
+					// Update number spinner values to match interval
+					updateNumberSpinner(position, mPrefs); 
+				}
 			}
 			
 			@Override
 			public void onNothingSelected(AdapterView<?> parent) { }
 		});
-		
-		// TODO: Verify setOnItemLongClickListener() produces desired results (as opposed something 
-		// like setOnLongClickListener() or a double-touch gesture.
-		mSpnNotifier.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-				// Activate/deactivate spinner
-				return true; 
+					
+		mSpnNumber.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override 
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				// Save currently selected number
+				switch (mSelectedInterval) {
+				case 0:
+					if (sSystemCallsToOnItemSelected > 1 || 
+							(sSystemCallsToOnItemSelected == 1 && mSelectedMinutes == 0)) {		// When app is first installed, Android calls onItemSelected() once upon creation, prior to user interaction (hence the second disjunctive condition). Thereafter, Android calls onItemSelected() twice prior to user interaction 
+						mSelectedMinutes = position;
+					}
+					sSystemCallsToOnItemSelected++; 
+					mPrefs.edit().putInt(SELECTED_MINUTES, mSelectedMinutes).commit();
+					break;
+				case 1: 
+					mSelectedHours = position;
+					mPrefs.edit().putInt(SELECTED_HOURS, mSelectedHours).commit();
+					break;
+				default:
+					mSelectedDays = position;
+					mPrefs.edit().putInt(SELECTED_DAYS, mSelectedDays).commit(); 
+				}
+				mNotifierIntervalAdapter.notifyDataSetChanged(); 	// Update NotifierIntervalAdapter from singular to plural and vice versa as needed
 			}
+			
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) { }
 		});
-	}
-	
-	private int getSelectedSpinnerItem(int position) {
-		String selection = (String) mSpnNotifier.getItemAtPosition(position); 
-		SpinnerSelectionConverter converter = new SpinnerSelectionConverter(selection); 
-		return converter.getSpinnerItemAsInt(); 
 	}
 	
 	@Override
@@ -108,12 +200,18 @@ public abstract class NotifierFragment extends Fragment {
 		
 		// TODO: Consider whether to save preferences and/or schedule alarm in onPause() or onStop()
 		// TODO: Consider refactoring into an AsyncTask
+		// Save selections of interval and number spinners
 		SharedPreferences prefs = getActivity().getSharedPreferences(SPINNER_PREFERENCES, 0); 
 		SharedPreferences.Editor editor = prefs.edit(); 
-		saveSpinnerSelection(editor, mSpnNotifier); 
+		editor.putInt(SELECTED_INTERVAL, mSelectedInterval) 
+		      .putInt(SELECTED_MINUTES, mSelectedMinutes)
+		      .putInt(SELECTED_HOURS, mSelectedHours)
+		      .putInt(SELECTED_DAYS, mSelectedDays)
+		      .commit(); 
 		
 		// TODO: Consider associating with a "set timer" button
-		scheduleSystemAlarm(); 
+		// TODO: Update scheduleSystemAlarm() in line with revised notifiers
+//		scheduleSystemAlarm(); 
 	}
 	
 	private void scheduleSystemAlarm() {		
@@ -132,11 +230,11 @@ public abstract class NotifierFragment extends Fragment {
 		    minutesInAdvance = 0;
 		
 		if (mSpinnerSelectionKey.startsWith("day")) {
-			daysInAdvance = mSpinnerValue; 
+			daysInAdvance = mSelectedMinutes; 
 		} else if (mSpinnerSelectionKey.startsWith("hour")) {
-			hoursInAdvance = mSpinnerValue;
+			hoursInAdvance = mSelectedMinutes;
 		} else {
-			minutesInAdvance = mSpinnerValue; 
+			minutesInAdvance = mSelectedMinutes; 
 		}
 		
 		// Calculate scheduling time for system alarm
@@ -167,20 +265,16 @@ public abstract class NotifierFragment extends Fragment {
 		context = null; 
 	}
 	
-	/*
-	 * Subclass should return resource value from R.java corresponding to string array 
-	 * used to populate subclass's spinner
-	 */
-	public abstract int getSpinnerValues(); 
-
-	/*
-	 * Subclass should use editor argument to persist its spinner value in SharedPreferences
-	 */
-	public abstract void saveSpinnerSelection(SharedPreferences.Editor editor, Spinner spinner); 
+	public int getSelectedMinutes() {
+		return mSelectedMinutes;
+	}
 	
-	/*
-	 * Subclass should return String key to its spinner value stored in SharedPreferences
-	 */
-	public abstract String getSpinnerSelectionKey(); 
+	public int getSelectedHours() {
+		return mSelectedHours;
+	}
+	
+	public int getSelectedDays() {
+		return mSelectedDays;
+	}
 	
 }
