9cf0c63101ed46c7735a05cad0b4cfe6c91037a2
Adam Brown <arbrow@gmail.com>
2014-04-30 13:19:47 -0700

    Issue #4 and Issue #8: Update persistence behavior and adapt system alarm logic accordingly

--- src/com/sfsweep/android/fragments/NotifierFragment.java
+++ src/com/sfsweep/android/fragments/NotifierFragment.java
@@ -40,19 +40,19 @@ public class NotifierFragment extends Fragment {
     private static final String ACTIVE_STATUS       = "spinner_active_status";
     private static final int    ALARM_REQUEST       = 1; 
     
-    private static int sSystemCallsToOnItemSelected = 0; 	// Flag neutralizes Android's firing onItemSelected() upon Spinner instantiation, rather than waiting for user interaction (see http://stackoverflow.com/questions/2562248/android-how-to-keep-onitemselected-from-firing-off-on-a-newly-instantiated-spin)
+    private static int sSystemCallsToOnItemSelected = 0; 	  // Flag neutralizes Android's firing onItemSelected() upon Spinner instantiation, rather than waiting for user interaction (see http://stackoverflow.com/questions/2562248/android-how-to-keep-onitemselected-from-firing-off-on-a-newly-instantiated-spin)
 	
-	private Spinner mSpnInterval; 
-	private Spinner mSpnNumber;
+	private Spinner mSpnInterval; 							  // Lets user select a minute, hour or day interval for alarm notification. Determines range of values displayed by number spinner
+	private Spinner mSpnNumber;								  // Lets user select a number value representing minutes, hours, or days
+	private int mSelectedInterval;							  // Values for mSelectedInterval, mSelectedMinutes, mSelectedHours and mSelectedDays represent indices (e.g., to recover the actual minute value selected, add 1 to mSelectedMinutes)
+	private int mSelectedMinutes;							 
+	private int mSelectedHours;
+	private int mSelectedDays; 
+	private boolean mActive = true; 						  // If true, alarm associated with notifier is active 
 	private NotifierIntervalAdapter mNotifierIntervalAdapter;
 	private NotifierNumberAdapter mNotifierMinutesAdapter;
 	private NotifierNumberAdapter mNotifierHoursAdapter;
 	private NotifierNumberAdapter mNotifierDaysAdapter; 
-	private int mSelectedInterval;
-	private int mSelectedMinutes;
-	private int mSelectedHours;
-	private int mSelectedDays; 
-	private boolean mActive = true; 
 	private SharedPreferences mPrefs; 
 	private OnScheduleAlarmCallbacks mScheduleListener; 
 
@@ -79,32 +79,29 @@ public class NotifierFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_notifier, parent, false); 
 		
 		setupWidgets(v); 
+		updateNumberSpinner(); 		// Number spinner updated to reflect interval selection (viz., minutes, hours, days)
 		setupListeners(); 
 		return v;
 	}
 	
 	private void setupWidgets(View v) {
-		// Restore active status
+		// Restore status of notifier (viz., active or inactive)
 		mPrefs  = getActivity().getSharedPreferences(SPINNER_PREFERENCES, 0); 
 		mActive = mPrefs.getBoolean(ACTIVE_STATUS, true); 
 		
 		// Set up interval spinner 
 		mSpnInterval = (Spinner) v.findViewById(R.id.spnInterval); 
-			
-		    // Implement using ArrayList to enable mutability (e.g., to change interval label 
-			// from singular to plural)
-		ArrayList<CharSequence> list = createAdapterArray(R.array.spn_options_intervals); 
+		ArrayList<CharSequence> list = createAdapterArray(R.array.spn_options_intervals);     	// Use of ArrayList enables mutability (e.g., changing interval label from singular to plural)
 		mNotifierIntervalAdapter = new NotifierIntervalAdapter(getActivity(), list, this); 
 		mNotifierIntervalAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item_custom);
 		mSpnInterval.setAdapter(mNotifierIntervalAdapter); 
 		
 			// Restore pre-selected interval, if any
-		int selectedInterval = mPrefs.getInt(SELECTED_INTERVAL, 0);
-		mSpnInterval.setSelection(selectedInterval); 
+		mSelectedInterval = mPrefs.getInt(SELECTED_INTERVAL, 0);
+		mSpnInterval.setSelection(mSelectedInterval); 
 		
 		// Set up number spinner
 		mSpnNumber = (Spinner) v.findViewById(R.id.spnNumber); 
-		updateNumberSpinner(selectedInterval, mPrefs);
 	}
 	
 	private ArrayList<CharSequence> createAdapterArray(int resourceId) {
@@ -114,10 +111,10 @@ public class NotifierFragment extends Fragment {
 		return list; 
 	}
 	
-	private void updateNumberSpinner(int selectedInterval, SharedPreferences prefs) {
+	private void updateNumberSpinner() {
 		// Retrieve pre-selected interval and create appropriate number adapter
 		ArrayAdapter<CharSequence> adapter; 
-		switch (selectedInterval) {
+		switch (mSelectedInterval) {
 		case 0: 	// Minute-based interval (passim)
 			if (mNotifierMinutesAdapter == null) {
 				ArrayList<CharSequence> list = createAdapterArray(R.array.spn_options_minutes);
@@ -145,11 +142,11 @@ public class NotifierFragment extends Fragment {
 		mSpnNumber.setAdapter(adapter); 
 		
 			// Restore pre-selected number, if any
-		mSelectedMinutes = prefs.getInt(SELECTED_MINUTES, 0);
-		mSelectedHours   = prefs.getInt(SELECTED_HOURS, 0);
-		mSelectedDays    = prefs.getInt(SELECTED_DAYS, 0); 
+		mSelectedMinutes = mPrefs.getInt(SELECTED_MINUTES, 0);
+		mSelectedHours   = mPrefs.getInt(SELECTED_HOURS, 0);
+		mSelectedDays    = mPrefs.getInt(SELECTED_DAYS, 0); 
 		int numberPosition = 0;
-		switch (selectedInterval) {
+		switch (mSelectedInterval) {
 		case 0:
 			numberPosition = mSelectedMinutes;
 			break;
@@ -167,30 +164,42 @@ public class NotifierFragment extends Fragment {
 	private void formatSpinners() {
 		LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) mSpnNumber.getLayoutParams();
 			
-			// Get dp-to-pixel conversion factor
+			// Calculate dp-to-pixel conversion factor
 		DisplayMetrics metrics = new DisplayMetrics(); 
 		getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics); 
 		final float logicalDensity = metrics.density;
-		int rightMarginPixels = (int) Math.ceil(4 * logicalDensity);	// dp values taken from trial-and-error results presented in res/layout/fragment_notifier 
+		int rightMarginPixels = (int) Math.ceil(4 * logicalDensity);	// dp values (here and in switch below) taken from trial-and-error results presented in res/layout/fragment_notifier 
 			
-			// Set margins
+			// Calculate and set margins
 		switch (mSelectedInterval) {
 		case 0:
-			// FIXME: Figure out why formatting is correct only after you select a value (eg, "12 minutes"), then select a different
-			// interval (eg, "hours"), and then return to the original interval 
-			if (mSelectedMinutes == 0)  params.setMargins(-1 * (int) Math.ceil(11 * logicalDensity), 0, rightMarginPixels, 0); 
-			else if (0 < mSelectedMinutes && mSelectedMinutes < 9)  params.setMargins(-1 * (int) Math.ceil(11 * logicalDensity), 0, rightMarginPixels, 0);
-			else params.setMargins((int) Math.ceil(5 * logicalDensity), 0, rightMarginPixels, 0); 
+			// FIXME: Formatting is correct only after (i) selecting a value (eg, "12 minutes"), (ii) selecting
+			// a different interval (eg, "hours"), and then (iii) returning to the original interval 
+			if (mSelectedMinutes == 0) {
+				params.setMargins(-1 * (int) Math.ceil(11 * logicalDensity), 0, rightMarginPixels, 0); 
+			} else if (0 < mSelectedMinutes && mSelectedMinutes < 9) {
+				params.setMargins(-1 * (int) Math.ceil(11 * logicalDensity), 0, rightMarginPixels, 0);
+			} else {
+				params.setMargins((int) Math.ceil(5 * logicalDensity), 0, rightMarginPixels, 0); 
+			}
 			break;
 		case 1:
-			if (mSelectedHours == 0)  params.setMargins((int) Math.ceil(10 * logicalDensity), 0, rightMarginPixels, 0); 
-			else if (0 < mSelectedHours && mSelectedHours < 9)  params.setMargins((int) Math.ceil(3 * logicalDensity), 0, rightMarginPixels, 0);
-			else params.setMargins((int) Math.ceil(21 * logicalDensity), 0, rightMarginPixels, 0); 
+			if (mSelectedHours == 0) { 
+				params.setMargins((int) Math.ceil(10 * logicalDensity), 0, rightMarginPixels, 0); 
+			} else if (0 < mSelectedHours && mSelectedHours < 9) {
+				params.setMargins((int) Math.ceil(3 * logicalDensity), 0, rightMarginPixels, 0);
+			} else {
+				params.setMargins((int) Math.ceil(21 * logicalDensity), 0, rightMarginPixels, 0); 
+			}
 			break;
 		default:
-			if (mSelectedDays == 0)  params.setMargins((int) Math.ceil(15 * logicalDensity), 0, rightMarginPixels, 0); 
-			else if (0 < mSelectedDays && mSelectedDays < 9)  params.setMargins((int) Math.ceil(3 * logicalDensity), 0, rightMarginPixels, 0);
-			else params.setMargins((int) Math.ceil(19 * logicalDensity), 0, rightMarginPixels, 0); 
+			if (mSelectedDays == 0) {
+				params.setMargins((int) Math.ceil(15 * logicalDensity), 0, rightMarginPixels, 0); 
+			} else if (0 < mSelectedDays && mSelectedDays < 9) {
+				params.setMargins((int) Math.ceil(3 * logicalDensity), 0, rightMarginPixels, 0);
+			} else {
+				params.setMargins((int) Math.ceil(19 * logicalDensity), 0, rightMarginPixels, 0); 
+			}
 		}
 		mSpnNumber.setLayoutParams(params); 
 	}
@@ -202,10 +211,10 @@ public class NotifierFragment extends Fragment {
 				if (position != mSelectedInterval) {
 					// Save currently selected interval
 					mSelectedInterval = position;
-					mPrefs.edit().putInt(SELECTED_INTERVAL, mSelectedInterval).commit(); 
+					mPrefs.edit().putInt(SELECTED_INTERVAL, position).commit(); 
 					
 					// Update number spinner values to match interval
-					updateNumberSpinner(position, mPrefs); 
+					updateNumberSpinner(); 
 				}
 			}
 			
@@ -218,7 +227,7 @@ public class NotifierFragment extends Fragment {
 			public boolean onLongClick(View v) {
 				mActive = !mActive; 
 				reformatOnActiveStatusChange(); 
-				// TODO: Activate or inactivate notification
+				// TODO: Activate or deactivate notification
 				return true;
 			}
 		});
@@ -256,7 +265,7 @@ public class NotifierFragment extends Fragment {
 			public boolean onLongClick(View v) {
 				mActive = !mActive;
 				reformatOnActiveStatusChange(); 
-				// TODO: Activate or inactivate notification
+				// TODO: Activate or deactivate notification
 				return true;
 			}
 		});
@@ -267,40 +276,29 @@ public class NotifierFragment extends Fragment {
 		Context  intervalContext   = mNotifierIntervalAdapter.getAdapterContext(); 
 		TextView numberTvSpnItem;
 		Context  numberContext; 
-		NotifierNumberAdapter adapter; 
 		
 		switch (mSelectedInterval) {
 		case 0: 
 			numberTvSpnItem = mNotifierMinutesAdapter.getTvSpnItem();
 			numberContext   = mNotifierMinutesAdapter.getAdapterContext(); 
-			adapter = mNotifierMinutesAdapter; 
 			break;
 		case 1:
 			numberTvSpnItem = mNotifierHoursAdapter.getTvSpnItem();
 			numberContext   = mNotifierHoursAdapter.getAdapterContext(); 
-			adapter = mNotifierHoursAdapter; 
 			break;
 		default:
 			numberTvSpnItem = mNotifierDaysAdapter.getTvSpnItem();
 			numberContext   = mNotifierDaysAdapter.getAdapterContext(); 
-			adapter = mNotifierDaysAdapter; 
 		}
 
 		if (mActive) { 	// Notifier is active
 			intervalTvSpnItem.setTextColor(intervalContext.getResources().getColor(R.color.platinum)); 
 			numberTvSpnItem.setTextColor(numberContext.getResources().getColor(R.color.platinum));
-			updateNumberArrayFormat(adapter); 
 		} else {
 			intervalTvSpnItem.setTextColor(intervalContext.getResources().getColor(R.color.sfsweep_orange)); 
 			numberTvSpnItem.setTextColor(numberContext.getResources().getColor(R.color.sfsweep_orange)); 
-			updateNumberArrayFormat(adapter); 
 			// TODO: Deactivate spinner
 		}
-	}
-	
-	private void updateNumberArrayFormat(NotifierNumberAdapter adapter) {
-		
-		// TODO
 	}
 	
 	@Override
@@ -310,12 +308,11 @@ public class NotifierFragment extends Fragment {
 		// TODO: Consider whether to save preferences and/or schedule alarm in onPause() or onStop()
 		// TODO: Consider refactoring into an AsyncTask
 		// Save selections of interval and number spinners
-		SharedPreferences prefs = getActivity().getSharedPreferences(SPINNER_PREFERENCES, 0); 
-		SharedPreferences.Editor editor = prefs.edit(); 
+		SharedPreferences.Editor editor = mPrefs.edit(); 
 		editor.putInt(SELECTED_INTERVAL, mSelectedInterval) 
-		      .putInt(SELECTED_MINUTES, mSelectedMinutes)
-		      .putInt(SELECTED_HOURS, mSelectedHours)
-		      .putInt(SELECTED_DAYS, mSelectedDays)
+		      .putInt(SELECTED_MINUTES,  mSelectedMinutes)
+		      .putInt(SELECTED_HOURS,    mSelectedHours)
+		      .putInt(SELECTED_DAYS,     mSelectedDays)
 		      .putBoolean(ACTIVE_STATUS, mActive)
 		      .commit(); 
 		
@@ -329,10 +326,16 @@ public class NotifierFragment extends Fragment {
 		Date nextSweepStart = mScheduleListener.onGetNextSweepStart();
 		Calendar calendar = Calendar.getInstance(Locale.US); 
 		calendar.setTime(nextSweepStart); 
+		Log.d("DEBUG", "******************* In scheduleSystemAlarm() *******************");
 		Log.d("DEBUG", "Calendar initially set at next sweep time: " + calendar.toString()); 
 		int sweepHour   = calendar.get(Calendar.HOUR_OF_DAY), 
 		    sweepDay    = calendar.get(Calendar.DAY_OF_YEAR); 
 		Log.d("DEBUG", "sweepHour is: " + sweepHour + ", and sweepDay is: " + sweepDay); 
+		
+		// Calculate actual value selected from number spinner (i.e., convert from index value)
+		int selectedMinute = mSelectedMinutes + 1,
+		    selectedHour   = mSelectedHours   + 1,
+		    selectedDay    = mSelectedDays    + 1;
 		
 		// Calculate system alarm schedule
 		int alarmMinute, alarmHour, alarmDay;  
@@ -341,7 +344,7 @@ public class NotifierFragment extends Fragment {
 		
 		switch (mSelectedInterval) {
 		case 0:
-			alarmMinute = 60 - mSelectedMinutes;
+			alarmMinute = 60 - selectedMinute;
 			if (sweepHour != 1)  alarmHour = sweepHour - 1;
 			else                 alarmHour = 24;
 			if (sweepHour != 24) alarmDay = sweepDay; 
@@ -349,16 +352,16 @@ public class NotifierFragment extends Fragment {
 			break;
 		case 1: 
 			alarmMinute = defaultMinute;
-			if (sweepHour < mSelectedHours) {
-				alarmHour = 24 - (mSelectedHours - sweepHour); 
+			if (sweepHour < selectedHour) {
+				alarmHour = 24 - (selectedHour - sweepHour); 
 				alarmDay  = sweepDay - 1;
 			}
-			else if (sweepHour == mSelectedHours) {
+			else if (sweepHour == selectedHour) {
 				alarmHour = 24; 
 				alarmDay  = sweepDay; 
 			}
 			else {
-				alarmHour = sweepHour - mSelectedHours; 
+				alarmHour = sweepHour - selectedHour; 
 				if (sweepHour != 24) alarmDay = sweepDay; 
 				else                 alarmDay = sweepDay - 1;
 			}
@@ -366,8 +369,10 @@ public class NotifierFragment extends Fragment {
 		default:
 			alarmMinute = defaultMinute;
 			alarmHour   = defaultHour;					
-			alarmDay    = sweepDay - mSelectedDays; 
+			alarmDay    = sweepDay - selectedDay; 
 		}
+		Log.d("DEBUG", "mSelectedMinutes: " + mSelectedMinutes + ", mSelectedHours: " + mSelectedHours + ", mSelectedDays: " + mSelectedDays); 
+		Log.d("DEBUG", "selectedMinute: " + selectedMinute + ", selectedHour: " + selectedHour + ", selectedDay: " + selectedDay); 
 		Log.d("DEBUG", "sweepMinute: " + defaultMinute + ", sweepHour: " + sweepHour + ", sweepDay: " + sweepDay); 
 		Log.d("DEBUG", "alarmMinute: " + alarmMinute + ", alarmHour: " + alarmHour + ", alarmDay: " + alarmDay); 
 		
@@ -393,6 +398,7 @@ public class NotifierFragment extends Fragment {
 		// TODO: Enable user to cancel system alarm
 		// TODO: Ensure re-selecting notifications cancels and replaces prior system alarm
 		context = null; 
+		Log.d("DEBUG", "**************************************");
 	}
 	
 	public void setActiveStatus(boolean active) {
