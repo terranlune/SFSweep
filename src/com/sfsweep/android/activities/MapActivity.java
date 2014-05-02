91a6c8076ef15ff95464b62d051c6c8f777e0596
KroneckerDelta <ledonnem3@gmail.com>
2014-05-01 20:13:04 -0700

    mapactivity with saved values/shared preferences but lines not redrawing

--- src/com/sfsweep/android/activities/MapActivity.java
+++ src/com/sfsweep/android/activities/MapActivity.java
@@ -1,41 +1,25 @@
 package com.sfsweep.android.activities;
 
-import java.io.File;
-
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
-
-
-import android.animation.LayoutTransition;
-
-
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.location.Location;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
-
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
-
-import android.widget.Button;
-
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -108,36 +92,76 @@ public class MapActivity extends FragmentActivity implements
 		//************************************************************************
 				//Mimi objects
 				//R always refers to xml data
-						Spinner spinner1= (Spinner) findViewById(R.id.spinner1); // find spinner1 in xml and attach it to spinner1 java
-						ArrayAdapter<CharSequence> spinnerAdapter1 = ArrayAdapter.createFromResource(
-								this, R.array.spinner1_opt, android.R.layout.simple_spinner_dropdown_item); //spinner1_opt is array list of positions in drop down Sun-Sat 
-						readItems(); //reads whatever is in the file that is written (which was the current position at the time (last selection) into spinneritem
-						spinner1.setSelection(spinnerItem); // use initial spinner position from text file. Use this position to set current value of spinner
+					final Spinner spinner1= (Spinner) findViewById(R.id.spinner1); // find spinner1 in xml and attach it to spinner1 java
+						ArrayAdapter<CharSequence> spinnerAdapter1 = ArrayAdapter.createFromResource( // got  ahandle to the adapter
+								this, R.array.spinner1_opt, android.R.layout.simple_spinner_item); //spinner1_opt is array list of positions in drop down Sun-Sat
+					
+						spinnerAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); //associating the adapter with the spinner
+						spinner1.setAdapter(spinnerAdapter1); 
+						//readItems(); //reads whatever is in the file that is written (which was the current position at the time (last selection) into spinneritem
+						  SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( getBaseContext());//declare prefs as a variable
+			              // above: give me the handle
+						
+						int spinselection = prefs.getInt("savedValue", 0); //RHS = get the retrieved value and LHS creates a variable to save the retrieved value
+						// "saved value" is the key that the int is the value is associated with
+						
+						spinner1.setSelection(spinselection); // use initial spinner position from text file. Use this position to set current value of spinner
 						spinner1.setAdapter(spinnerAdapter1); // have spinner in place and keeping track of what position it is in
-						spinner1.setOnItemSelectedListener(new OnItemSelectedListener() { //Have spinner do something when you select item
-							
-							 public void onItemSelected(AdapterView<?> arg0, View arg1,
-					                   int pos, long arg3) {
-								 Spinner spinner1= (Spinner) findViewById(R.id.spinner1); //Get handle to spinner1 in xml file
-								 spinnerItem=pos;//store user selection in spinnerItem variable
-								 writeItems();//write user selection to file save value of spinner item which is position to a file 
-								 if(pos ==0){  // conditional: based on selected spinner value it will execute different code. If position =0 
-									 			//then it is first option in spinner menu=> heatmap mode
-									 Log.d("DEBUG", "Hello from on if_heatmap");
-								     mapAdapter.setModeHeatmap() ; // calls 
-								    }
-								    else 
-								    	{mapAdapter.setModeWeekday(spinner1.getSelectedItem().toString());
-								    	Log.d("DEBUG", "Hello from weekdayMode");
-								    	}
-								    Log.d("DEBUG", "Hello from on item selected");				
-							 }
-							@Override
-							public void onNothingSelected(AdapterView<?> arg0) {
-								// TODO Auto-generated method stub
-								
-								
-							}	});
+						//Have spinner do something when you select item
+
+						spinner1.setOnItemSelectedListener(new OnItemSelectedListener() { 
+					
+						 
+							//spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {           
+						           @Override
+						           public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+						        	   // can only store primitive data types like ints, strings(only non primitive),boolean, float, double, char
+						        	   // the object in this case is the shared preference. will get a handle to the shared pref object. 
+						        	   //common pattern for already accessible android object create from the sharedpref class
+						               SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( getBaseContext());//declare prefs as a variable
+						              // above: give me the handle
+						               SharedPreferences.Editor prefEditor = prefs.edit();// intermediary abstraction to hide implementation. We share saved prefs in batches
+						               prefEditor.putInt("savedValue",position);// what's returned from spinner.getsel will be stored
+						               prefEditor.commit();
+						               String savedValue=spinner1.getSelectedItem().toString();
+
+						               for(int i=0;i<7;i++)
+						               if(savedValue.equals(spinner1.getItemAtPosition(i).toString()))
+						               {
+						                   spinner1.setSelection(i);
+						                   break;
+						               }
+						           }
+						           @Override
+						           public void onNothingSelected(AdapterView<?> parent){}
+						    });
+	
+						
+						
+						
+						
+//						public void onItemSelected(AdapterView<?> arg0, View arg1,
+//					                   int pos, long arg) {
+//								 Spinner spinner1= (Spinner) findViewById(R.id.spinner1); //Get handle to spinner1 in xml file
+//								 spinnerItem=pos;//store user selection in spinnerItem variable
+//								 //writeItems();//write user selection to file save value of spinner item which is position to a file 
+//								 if(pos ==0){  // conditional: based on selected spinner value it will execute different code. If position =0 
+//									 			//then it is first option in spinner menu=> heatmap mode
+//									 Log.d("DEBUG", "Hello from on if_heatmap");
+//								     mapAdapter.setModeHeatmap() ; // calls 
+//								    }
+//								    else 
+//								    	{mapAdapter.setModeWeekday(spinner1.getSelectedItem().toString());
+//								    	Log.d("DEBUG", "Hello from weekdayMode");
+//								    	}
+//								    Log.d("DEBUG", "Hello from on item selected");				
+//							 }
+//							@Override
+//							public void onNothingSelected(AdapterView<?> arg0) { //super type is a method inherited form the parent class
+//								// TODO Auto-generated method stub
+//							}
+//								
+//								});
 
 		
 		mLocationClient = new LocationClient(this, this, this);
@@ -179,33 +203,33 @@ public class MapActivity extends FragmentActivity implements
 	}
 	
 	//*******************************************************************************
-		public class Spinner1Activity extends Activity {
-			
-		}
-				
-		 private void readItems() {
-			   File filesDir = getFilesDir();
-			   File spinnerFile = new File(filesDir, "spinner.tx"); //open the file on device 
-			 
-			   try { 
-				   // read the text from file and puts it in the spinneritems variable. which is an int showing currently selected value of the spinner
-				   spinnerItem = Integer.parseInt(FileUtils.readFileToString(spinnerFile));
-			   } catch (IOException e) {
-				   spinnerItem = 7 ; //  set an arbitrary default value... 
-				   //spinnerItem = new String(); //left is what is getting assigned. right is what you are putting there
-				   
-			   }
-
-		}
-		   private void writeItems() {
-			   File filesDir = getFilesDir();
-			   File spinnerFile = new File(filesDir, "spinner.tx"); //opening a file on the device
-			   try {
-				   FileUtils.writeStringToFile(spinnerFile, String.valueOf(spinnerItem)); //writing text to the file
-			   } catch (IOException e) {
-				   e.printStackTrace();
-			   }	   
-		   }
+//		public class Spinner1Activity extends Activity {
+//			
+//		}
+//				
+//		 private void readItems() {
+//			   File filesDir = getFilesDir();
+//			   File spinnerFile = new File(filesDir, "spinner.tx"); //open the file on device 
+//			 
+//			   try { 
+//				   // read the text from file and puts it in the spinneritems variable. which is an int showing currently selected value of the spinner
+//				   spinnerItem = Integer.parseInt(FileUtils.readFileToString(spinnerFile));
+//			   } catch (IOException e) {
+//				   spinnerItem = 7 ; //  set an arbitrary default value... 
+//				   //spinnerItem = new String(); //left is what is getting assigned. right is what you are putting there
+//				   
+//			   }
+//
+//		}
+//		   private void writeItems() {
+//			   File filesDir = getFilesDir();
+//			   File spinnerFile = new File(filesDir, "spinner.tx"); //opening a file on the device
+//			   try {
+//				   FileUtils.writeStringToFile(spinnerFile, String.valueOf(spinnerItem)); //writing text to the file
+//			   } catch (IOException e) {
+//				   e.printStackTrace();
+//			   }	   
+//		   }
 		
 		   //********************************************************************************************
 
